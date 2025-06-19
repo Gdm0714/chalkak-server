7df9d2b86fd5f6acb90f4b5c57e846074d3ec1a0
Gdm0714 <ehdals45454@naver.com>
2025-06-19 17:20:48 +0900

    실행에 필요한 Application파일 추가

---
+++ src/main/java/com/min/chalkakserver/ChalkakServerApplication.java
@@ -0,0 +1,13 @@
+package com.min.chalkakserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalkakServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChalkakServerApplication.class, args);
+    }
+
+}
